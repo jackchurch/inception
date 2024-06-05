28e131a708825f22eb32668427d8486b1c622c98
Jack Church <jchurch@jchurch.42.fr>
2024-06-05 20:17:37 +0930

    Env file check added.

--- srcs/requirements/tools/pre_build.sh
+++ srcs/requirements/tools/pre_build.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# make database folder if it doens't exist
-if [ ! -d "/home/${USER}/data/database" ];
+if [ -f "srcs/.env" ];
 then
-    mkdir -p /home/${USER}/data/database
+    # make database folder if it doens't exist
+    if [ ! -d "/home/${USER}/data/database" ];
+    then
+        mkdir -p /home/${USER}/data/database
+    fi
+
+    # make web folder if it doens't exist
+    if [ ! -d "/home/${USER}/data/web" ];
+    then
+        mkdir -p /home/${USER}/data/web
+    fi
+
+    # Update username in docker-compose file
+    sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/docker-compose.yml
+
+    # Update username in nginx config for the domain "username.42.fr"
+    sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/requirements/nginx/conf/nginx.conf
+
+
+    # Update username for domain in .env file"
+    sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/.env
+else
+    echo 
+    echo
+    echo "Error: srcs/.env file not found. Did you remember to make it from the sample provided?"
+    echo
 fi
-
-# make web folder if it doens't exist
-if [ ! -d "/home/${USER}/data/web" ];
-then
-    mkdir -p /home/${USER}/data/web
-fi
-
-# Update username in docker-compose file
-sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/docker-compose.yml
-
-# Update username in nginx config for the domain "username.42.fr"
-sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/requirements/nginx/conf/nginx.conf
-
-
-# Update username for domain in .env file"
-sed -i "s|SCRIPT_TO_CHANGE_ME|${USER}|g" srcs/.env
